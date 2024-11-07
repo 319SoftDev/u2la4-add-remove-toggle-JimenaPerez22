6b4e4cd21d1ecdd7e1d53137115579ac555ae453
JimenaPerez22 <jimenaperezmorales12@gmail.com>
2024-11-07 10:35:57 -0500

    Updated JAVASCRIPT

--- script.js
+++ script.js
@@ -40,13 +40,13 @@ sendButton.addEventListener("click",closeReplyMessage);
 //opening Michaels Messanger
 const openButton = document.querySelector("#open-button");
 const inboxMessage = document.querySelector("#inbox-message");
-const actionButton = document.querySelector("#action-button");
+const markButton = document.querySelector("#mark-button");
 
 
 const openMessanger = () =>{
     heading.classList.add("is-read");
     inboxMessage.classList.remove("hidden");
-    actionButton.classList.remove("hidden");
+    markButton.classList.remove("hidden");
 }
 
 openButton.addEventListener("click", openMessanger);
@@ -58,7 +58,33 @@ const close = document.querySelector("#close");
 const closeMessanger = () =>{
     heading.classList.remove("is-read");
     inboxMessage.classList.add("hidden");
-    actionButton.classList.add("hidden");
+    markButton.classList.add("hidden");
 }
 
-close.addEventListener("click",closeMessanger);
+close.addEventListener("click", closeMessanger);
+
+//hide the inbox message + turn message background white
+const hideInboxMessage = () =>{
+    markButton.classList.add("hidden");
+    openButton.classList.add("hidden");
+    replyButton.classList.add("hidden");
+    inboxMessage.classList.add("hidden");
+}
+
+markButton.addEventListener("click", hideInboxMessage);
+
+
+// making the toggle of the Michaels Messenger
+const checkbox = document.querySelector("#checkbox");
+const actionButtons = document.querySelector("#ation-buttons");
+const inbox = document.querySelector("#inbox");
+
+const changeInbox = () =>{
+    inbox.classList.toggle("is-selected");
+    actionButtons.classList.toggle("hidden");
+}
+
+checkbox.addEventListener("click", changeInbox);
+
+
+//Medium activity
